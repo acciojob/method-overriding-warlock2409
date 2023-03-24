8c285f0bf27ee7a88299b984b1e240c7464cbf40
rajaram <rajaram@runloyal.com>
2023-03-24 16:46:43 +0530

    overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+
+       String ans= obj.meth();
+       System.out.print(ans);
+    }
 }
